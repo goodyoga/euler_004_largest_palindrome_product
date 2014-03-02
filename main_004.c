032b3a0bb4f5800872da59d24ce0b0900091669f
goodyoga <goodyoga.goodyoga@gmail.com>
2014-03-02 11:35:36 +0900

    settings and doxygen

--- main_004.c
+++ main_004.c
@@ -6,23 +6,98 @@
 #include <unistd.h>   /* getopt */
 
 /**
- * @file
+ * @file main_004.c
  * @addtogroup  EULER_004  Euler_004_largetst_palindrome_product
  * @{
  */
 
+/// @enum enumeration values for default.
+typedef enum {
+	/// max value of the element of palindrome. 
+	DEFAULT_MAX = 999, 
+	
+	/// min value of the element of alindrome.
+	DEFAULT_MIN = 100,
+} GLOBAL_CONST;
+
+/// @def BUFF_SIZE
+///  max length of digits in ascii string. 
+#define BUFF_SIZE   128
+
+
+
+
+
+/**
+ * @brief Largest palindrome product
+ * 
+ *   member | default
+ *   -------|----------
+ *   all    | 0
+ *   max    | DEFAULT_MAX
+ *   min    | DEFAULT_MIN
+ *   
+ * @param argc number of argments incl. program name.
+ * @param argv -h shows help
+ * @param argv -n=&lt;number&gt; sets max value for each element of palindrome as &lt;number&gt;
+ * @param argv -a sets all to 1, prints all debug printf().
+ * @param argv
+ * 
+ * @return EXIT_SUCCESS on success
+ * @return -1 on error
+ * 
+ * @pre
+ * 
+ * @post
+ * 
+ * @test ready to test
+ * 
+ * @bug none
+ * 
+ */
+int main(int argc, char **argv);
+
+
+/**
+ * @brief show help
+ */
 void usage(void);
-int check(unsigned long long int cur);
+
+
 
 /**
  * @brief check() checks if cur is a palindrome.
+ * 
  * @param cur a number to check
- * @return 1:is palindrome, or 0:not
+ * @return 1 is palindrome
+ * @return 0 is not palindrome
+ * 
+ * @pre none
+ * 
+ * @post none
+ * 
+ * @note 
+ * this function can check max length as set in @ref BUFF_SIZE
+ * @test not ready to test.
+ * 
+ * @bug unknown
+ * 
  */
+int check(unsigned long long int cur);
+
+/**
+ * @}
+ */
+
+
+
+/***********************************
+ * Implementation
+ ***********************************/
+
+
 int check(unsigned long long int cur)
 {
-#define BUFF_SIZE   128
-	
     char buff[BUFF_SIZE], reverse[BUFF_SIZE];
     size_t len;
     unsigned int i;
@@ -35,13 +110,8 @@ int check(unsigned long long int cur)
     return (0 == strncmp(buff, reverse, len));
 }
 
-/**
- * @brief Largest palindrome product
- * 
- * @param argc
- * @param argv
- * @return
- */
+
+
 int main(int argc, char **argv)
 {
     unsigned long long int max,min;
@@ -50,8 +120,8 @@ int main(int argc, char **argv)
     int opt, all = 0;
     char *p;
     
-    max = 999;
-    min = 100;
+    max = DEFAULT_MAX;
+    min = DEFAULT_MIN;
 
     while ( -1 != (opt = getopt(argc, argv, "ahn:")) )
     {
@@ -101,9 +171,6 @@ int main(int argc, char **argv)
 }
 
 
-/**
- * @brief show help
- */
 void usage(void)
 {
     printf("    http://projecteuler.net/problem=4\n");
@@ -117,8 +184,4 @@ void usage(void)
            "    -n <number>: solve this with max <number>\n");
     return;
 }
-
-/**
- * @}
- */
 
